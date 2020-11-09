c2243dbbb39d3ad35b342fcca1ad8d6e23a465f3
Liang Liu <liuliang@danfoss.com>
2020-11-09 13:16:06 +0800

    Init project

---
+++ photos2pdf.py
@@ -0,0 +1,22 @@
+from fpdf import FPDF
+from PIL import Image
+import os
+
+def makePdf(pdfFileName, listPages):
+	cover = Image.open(listPages[0])
+	width, height = cover.size
+	pdf = FPDF(unit = "pt", format = [width, height])
+	for page in listPages:
+		pdf.add_page()
+		pdf.image(page, 0, 0)
+
+	pdf.output(pdfFileName, "F")
+
+makePdf("result.pdf", ["./test_picture/" + imgFileName for imgFileName in os.listdir('./test_picture') \
+					   if imgFileName.endswith("png")])
+
+
+# for imgFileName in os.listdir(r'C:\Users\U332588\OneDrive - Danfoss\Pictures'):
+#     if imgFileName.endswith("png"):
+#         # print('C:\Users\U332588\OneDrive - Danfoss\Pictures' + imgFileName)
+#         print(os.path.abspath(imgFileName))
