cbcb7b69888aa62e26e7a0b91e75303fc8c96852
MrSprint <864047435@qq.com>
2020-11-26 23:18:52 +0800

    add headers

--- catch_from_website.py
+++ catch_from_website.py
@@ -2,25 +2,29 @@ import ssl
 import re
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
-from selenium import webdriver 
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities 
 
 total_pitcures = 0
 picture_list = list()
+headers = {'User-Agent': '(Linux;U;Android 2.3;en-us;Nexus One Build/FRF91)AppleWebKit/999+(KHTML, like Gecko)Version/4.0 Mobile Safari/999.9'}
 def screen_shot(url):
     global total_pitcures
     global picture_list
-
+    global headers
     png_name = "picture/" + str(total_pitcures) + ".png"
+    dcap = dict(DesiredCapabilities.PHANTOMJS)
+    dcap["phantomjs.page.settings.userAgent"] = headers['User-Agent']
     total_pitcures += 1
     picture_list.append(png_name)
-    brower = webdriver.PhantomJS()
+    brower = webdriver.PhantomJS(desired_capabilities=dcap)
     brower.get(url)
     brower.maximize_window()
     brower.save_screenshot(png_name)
     brower.close()
 
 def get_link(url):
-    headers = {'User-Agent': '(Linux;U;Android 2.3;en-us;Nexus One Build/FRF91)AppleWebKit/999+(KHTML, like Gecko)Version/4.0 Mobile Safari/999.9'}
+    global headers
     context = ssl._create_unverified_context()
     request = urllib.request.Request(url, headers=headers)
     response = urllib.request.urlopen(request,context=context)
@@ -31,15 +35,13 @@ def get_link(url):
         print(tag.get('href', None), end = "\n")
         return tag.get('href', None)
 
-
 if __name__ == '__main__':
     url ="https://m.xialashimanhua.com/manhua/huibuqudexiatian/1335161.html"
     screen_shot(url)
-    while url != "https://m.xialashimanhua.com/manhua/huibuqudexiatian/1335232-133.html":
+    while total_pitcures < 10 and url != "https://m.xialashimanhua.com/manhua/huibuqudexiatian/1335232-133.html":
         url = get_link(url)
         screen_shot(url)
 
-# from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
-# 
+
+
 
